0e3fb86c765c917630c794998621cdb3b4b44103
chobler <chobler@users.noreply.github.com>
2015-12-15 12:36:56 +0000

    Manually adding hyde

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-python -c 'import os;import sys;output = lambda(x) : sys.stdout.write(x + "\n"); paths = os.environ["PATH"].split(":") ; listdir = lambda(p) : os.listdir(p) if os.path.isdir(p) else [ ] ; isfile = lambda(x) : True if os.path.isfile(os.path.join(x[0],x[1])) else False ; isexe = lambda(x) : True if os.access(os.path.join(x[0],x[1]), os.X_OK) else False ; map(output,[ os.path.join(p,f) for p in paths for f in listdir(p) if isfile((p,f)) and isexe((p,f)) ])'
+apt-get update -y || true
+yum update -y || true
 
 hugo --theme=hyde
